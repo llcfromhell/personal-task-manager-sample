325e899168626f3e5c44fe097698de719139d8f0
Lourenço Lima da Cunha <Loureno Lima da Cunha>
2017-10-26 23:06:48 -0200

    adding jawsdb info

--- util/conecta.php
+++ util/conecta.php
@@ -5,7 +5,7 @@
  * Date: 05/10/2017
  * Time: 14:17
  */
-$conexao = mysqli_connect("localhost", "root", "", "projetoIntegrador");
+$conexao = mysqli_connect("ko86t9azcob3a2f9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "zph9un2swyi9h0bk", "pc2uyih74byti1le", "ev66y9c3ie76deqe");
 
 
 if (!$conexao->set_charset("utf8")) {
